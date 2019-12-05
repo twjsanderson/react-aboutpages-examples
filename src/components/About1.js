ebaf882db9ac7e8163cac6f4ac85d0296280cfd5
vagrant <tom.w.j.sanderson@gmail.com>
2019-12-05 23:02:28 +0000

    created last about page

--- src/components/About1.js
+++ src/components/About1.js
@@ -1,24 +1,41 @@
 import React , { Fragment } from 'react';
 import Nav1 from './Navbar';
 import { Container, Row, Col } from 'react-bootstrap';
-
+import landscape from '../assets/landscape.jpeg';
 // 
 
 const About1 = () => {
   return (
     <Fragment>
       <Nav1 />
-      <Container fluid style={{ height: 1000 }}> 
-        <Row className="h-50 justify-content-center align-items-center">
-          <Col md={4} className="text-center">
-            <h1>Main Title</h1>
+      <Container>
+        <Row className="p-3 justify-content-center align-items-center">
+          <Col md={8} className="text-center">
+              <h1 className="text-black">Main Title</h1>
+              <p className="text-black pt-3">
+                  fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies 
+                  tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam 
+                  eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a 
+                  scelerisque purus semper eget duis
+              </p>
+
           </Col>
-        </Row>
-        <Row className="h-50 justify-content-center align-items-center">
-          <Col md={4} className="text-center">
-            <p className="text-warning">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-          </Col>
+          </Row>
+          <Row className="h-100 p-3 justify-content-center align-items-center">
+            <Col md={6} className='text-center'>
+              <h3>Subtitle</h3>
+              <img 
+                src={landscape}
+                width="100%"
+                
+              />
+              <p className="text-black pt-3">
+                  fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies 
+                  tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam 
+                  eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a 
+                  scelerisque purus semper eget duis
+              </p>
+            </Col>
         </Row>
       </Container>
     </Fragment>
--- src/components/About2.js
+++ src/components/About2.js
@@ -11,6 +11,17 @@ const About2 = () => {
     <Fragment>
         <Nav1 />
         <Container>
+            <Row className="p-3 justify-content-center align-items-center">
+                <Col md={6} className="text-center">
+                    <h1 className="text-black">Main Title</h1>
+                    <p className="text-black pt-3">
+                        fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies 
+                        tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam 
+                        eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a 
+                        scelerisque purus semper eget duis
+                    </p>
+                </Col>
+            </Row>   
             <Row className="m-2" style={{ border: 'solid 1px black' }}>
                 <Col md={4} style={{ paddingRight: 0, paddingLeft: 0, borderRight: 'solid 1px black' }}>
                     <img 
@@ -19,12 +30,14 @@ const About2 = () => {
                         height="100%"
                     />
                 </Col>
-                <Col md={8} className="d-flex flex-column justify-content-center align-items-center" style={{ paddingRight: 0, paddingLeft: 0 }}>
-                    <h1 className="py-2">Title</h1>
-                    <h3 className="py-2">Subtitle</h3>
-                    <p className="my-auto p-2">lorem ipsim fjh fklafk dsmklfl mds,lmfkd 'sm fd
-                        fdms,la fdsfkl mdsla fmlsamf ldsam fdfmdsa fmsd mjkk
-                        fdskafmds
+                <Col md={8} className="d-flex flex-column align-items-start" style={{ paddingRight: 0, paddingLeft: 0 }}>
+                    <h1 className="p-2">Title</h1>
+                    <h3 className="p-2">Subtitle</h3>
+                    <p className="my-auto p-2">
+                        Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies 
+                        tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam 
+                        eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a 
+                        scelerisque purus semper eget duis
                     </p>
                     <small className="mt-auto py-3 text-center bg-dark text-white w-100">this is small</small>
                 </Col>
@@ -37,12 +50,14 @@ const About2 = () => {
                         height="100%"
                     />
                 </Col>
-                <Col md={8} className="d-flex flex-column justify-content-center align-items-center" style={{ paddingRight: 0, paddingLeft: 0 }}>
-                    <h1 className="py-2">Title</h1>
-                    <h3 className="py-2">Subtitle</h3>
-                    <p className="my-auto p-2">lorem ipsim fjh fklafk dsmklfl mds,lmfkd 'sm fd
-                        fdms,la fdsfkl mdsla fmlsamf ldsam fdfmdsa fmsd mjkk
-                        fdskafmds
+                <Col md={8} className="d-flex flex-column align-items-start" style={{ paddingRight: 0, paddingLeft: 0 }}>
+                    <h1 className="p-2">Title</h1>
+                    <h3 className="p-2">Subtitle</h3>
+                    <p className="my-auto p-2">
+                        Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies 
+                        tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam 
+                        eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a 
+                        scelerisque purus semper eget duis
                     </p>
                     <small className="mt-auto py-3 text-center bg-dark text-white w-100">this is small</small>
                 </Col>
